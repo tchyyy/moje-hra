e763f8a3c33463eec822fa90ff0317cb791cc46d
tchyyy <david.tichy@skolakrizik.cz>
2025-05-11 19:33:28 +0200

    gameska
    pridani souboje se tremi monstry

--- Programovani gameska - DT.cpp
+++ Programovani gameska - DT.cpp
@@ -70,19 +70,19 @@ int main() {
         }
     }
 
-            cout << "Vitej ve hre, " << herniJmeno << "!" << endl;
-            cout << "Priprav se, budes bojovat!" << endl;
+    cout << "Vitej ve hre, " << herniJmeno << "!" << endl;
+    cout << "Priprav se, budes bojovat!" << endl;
 
-            string postavy[] = {"M", "2xM", "V", "3xM", "MB", "V", "M", "HB"};
+    string postavy[] = {"M", "2xM", "V", "3xM", "MB", "V", "M", "HB"};
 
-        for (int i = 0; i < 8; i++) {
-            string aktualni = postavy[i];
-            cout << endl << "Setkal jsi se s: " << aktualni << endl;
+    for (int i = 0; i < 8; i++) {
+        string aktualni = postavy[i];
+        cout << endl << "Setkal jsi se s: " << aktualni << endl;
 
         if (aktualni == "M") {
-                int zivotMonstra = 3;
-                while (zivotMonstra > 0 && zivoty > 0) {
-                    cout << "Utocis za " << utok << " zraneni." << endl;
+            int zivotMonstra = 3;
+            while (zivotMonstra > 0 && zivoty > 0) {
+                cout << "Utocis za " << utok << " zraneni." << endl;
                 zivotMonstra -= utok;
 
                 if (zivotMonstra <= 0) {
@@ -90,7 +90,7 @@ int main() {
                     break;
                 }
 
-                    cout << "Monstrum vraci uder! Ubrati 1 zivot." << endl;
+                cout << "Monstrum vraci uder! Ubrati 1 zivot." << endl;
                 zivoty--;
 
                 if (zivoty <= 0) {
@@ -98,15 +98,15 @@ int main() {
                     return 0;
                 }
 
-                    cout << "Mas " << zivoty << " zivotu zbyva." << endl;
+                cout << "Mas " << zivoty << " zivotu zbyva." << endl;
             }
 
         } else if (aktualni == "2xM") {
-                int zivotM1 = 3, zivotM2 = 3;
-                while ((zivotM1 > 0 || zivotM2 > 0) && zivoty > 0) {
-                    cout << "Vyber monstrum do kteryho pujdes (1 nebo 2): ";
+            int zivotM1 = 3, zivotM2 = 3;
+            while ((zivotM1 > 0 || zivotM2 > 0) && zivoty > 0) {
+                cout << "Vyber monstrum do kteryho pujdes (1 nebo 2): ";
                 int volba;
-                    cin >> volba;
+                cin >> volba;
 
                 if (volba == 1 && zivotM1 > 0) {
                     zivotM1 -= utok;
@@ -118,7 +118,6 @@ int main() {
                     cout << "Ty chces zabijet neco co si zabil?. :D" << endl;
                     continue;
                 }
-
 
                 if (zivotM1 > 0) {
                     cout << "Monstrum 1 te zasahuje za 1 zivot." << endl;
@@ -134,16 +133,59 @@ int main() {
                     return 0;
                 }
 
-                    cout << "Mas " << zivoty << " zivotu zbyva." << endl;
+                cout << "Mas " << zivoty << " zivotu zbyva." << endl;
             }
-                    cout << "Porazil jsi dve monstra!" << endl;
+            cout << "Porazil jsi dve monstra!" << endl;
 
-            } else if (aktualni == "V") {
-                    cout << "Dorazil jsi do vesnice. Tady si odpocines od monster a doplnis manu." << endl;
+        } else if (aktualni == "3xM") {
+            int zivotM1 = 3, zivotM2 = 3, zivotM3 = 3;
+            while ((zivotM1 > 0 || zivotM2 > 0 || zivotM3 > 0) && zivoty > 0) {
+                cout << "Vyber monstrum do kteryho pujdes (1, 2 nebo 3): ";
+                int volba;
+                cin >> volba;
+
+                if (volba == 1 && zivotM1 > 0) {
+                    zivotM1 -= utok;
+                    cout << "Utocis na monstrum 1. Zivoty: " << max(0, zivotM1) << endl;
+                } else if (volba == 2 && zivotM2 > 0) {
+                    zivotM2 -= utok;
+                    cout << "Utocis na monstrum 2. Zivoty: " << max(0, zivotM2) << endl;
+                } else if (volba == 3 && zivotM3 > 0) {
+                    zivotM3 -= utok;
+                    cout << "Utocis na monstrum 3. Zivoty: " << max(0, zivotM3) << endl;
+                } else {
+                    cout << "Ty chces zabijet neco co si zabil?. :D" << endl;
+                    continue;
+                }
+
+                if (zivotM1 > 0) {
+                    cout << "Monstrum 1 te zasahuje za 1 zivot." << endl;
+                    zivoty--;
+                }
+                if (zivotM2 > 0) {
+                    cout << "Monstrum 2 te zasahuje za 1 zivot." << endl;
+                    zivoty--;
+                }
+                if (zivotM3 > 0) {
+                    cout << "Monstrum 3 te zasahuje za 1 zivot." << endl;
+                    zivoty--;
+                }
+
+                if (zivoty <= 0) {
+                    cout << "Zemrel jsi. Konec hry." << endl;
+                    return 0;
+                }
+
+                cout << "Mas " << zivoty << " zivotu zbyva." << endl;
+            }
+            cout << "Porazil jsi tri monstra!" << endl;
+
+        } else if (aktualni == "V") {
+            cout << "Dorazil jsi do vesnice. Tady si odpocines od monster a doplnis manu." << endl;
             zivoty = maxZivoty;
             mana = maxMana;
-            } else {
-                    cout << "Zatim mas klidos." << endl;
+        } else {
+            cout << "Zatim mas klidos." << endl;
         }
     }
 
